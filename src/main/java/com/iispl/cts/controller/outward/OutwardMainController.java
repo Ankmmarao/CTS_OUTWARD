bbf38d5d45a3c739a9e9b1e9b4c6b4b82696c9c1
yugesherukula0 <yugesherukala@gmail.com>
2026-09-09 09:49:55 +0530

    formating the zul pages

---
+++ src/main/java/com/iispl/cts/controller/outward/OutwardMainController.java
@@ -0,0 +1,8 @@
+package com.iispl.cts.controller.outward;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.util.GenericForwardComposer;
+
+public class OutwardMainController extends GenericForwardComposer<Component>{
+
+}
